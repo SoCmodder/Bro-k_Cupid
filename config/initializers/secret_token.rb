13de5bf02eeb681846ea0dd8ef2b629a28e120d8
SoCmodder <socmodder@gmail.com>
2013-09-22 16:45:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BroKCupid::Application.config.secret_token = 'c198d168ff4024ac0ed092267784cf6c0e8ec462c2d28da95f6cb50c3a5aa481e2e8af40add5dd610c10ea08538fd82e78c30d11cb6a2d73471450dd9e56101b'
