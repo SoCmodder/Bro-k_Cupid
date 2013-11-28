c3afe11a47d43d3af1a7dbc030fb73b1f7104167
SoCmodder <socmodder@gmail.com>
2013-11-27 20:51:50 -0600

    Questions can now be answered.

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,16 +1,13 @@
 class UserAnswersController < ApplicationController
     #rescue_from ActiveRecord::RecordNotUnique, with: :find_and_replace_answer
   def create
-
-
-
     if UserAnswer.exists?(:user_id=>params[:u_id], :question_id=>params[:q_id])
         @user_answer = UserAnswer.where(:user_id => params[:u_id], :question_id => params[:q_id])[0]
-        @user_answer[:answer] = params[:ans]
+        @user_answer[:answer_id] = params[:ans]
         @user_answer[:importance] = params[:imp]
     else
         @user = User.find(params[:u_id])
-        @user_answer = @user.user_answers.build(:user_id => params[:u_id], :question_id => params[:q_id], :answer => params[:ans], :importance => params[:imp])
+        @user_answer = @user.user_answers.build(:user_id => params[:u_id], :question_id => params[:q_id], :answer_id => params[:ans], :importance => params[:imp])
     end
 
     if @user_answer.valid?
