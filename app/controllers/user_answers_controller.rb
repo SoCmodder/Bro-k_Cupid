129df54d387190f03ee944c3f5e9cf4b6f4aa85f
P <pash@ubuntu.(none)>
2013-11-29 12:02:48 -0800

    NOW users can answer questions - need to fix style

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,29 +1,31 @@
 class UserAnswersController < ApplicationController
-    #rescue_from ActiveRecord::RecordNotUnique, with: :find_and_replace_answer
-  def create
-    if UserAnswer.exists?(:user_id=>params[:u_id], :question_id=>params[:q_id])
-        @user_answer = UserAnswer.where(:user_id => params[:u_id], :question_id => params[:q_id])[0]
-        @user_answer[:answer_id] = params[:ans]
-        @user_answer[:importance] = params[:imp]
-    else
-        @user = User.find(params[:u_id])
-        @user_answer = @user.user_answers.build(:user_id => params[:u_id], :question_id => params[:q_id], :answer_id => params[:ans], :importance => params[:imp])
-    end
 
+    def create
+    logger.debug "Params: #{params.inspect}"
+    if UserAnswer.exists?(:user_id=>params[:user_answer][:user_id], :question_id=>params[:user_answer][:question_id])
+        flash[:notice] = "Exists"
+        @user_answer = UserAnswer.where(:user_id => params[:user_answer][:user_id], :question_id => params[:user_answer][:question_id])[0]
+        @user_answer[:answer] = params[:user_answer][:answer]
+        @user_answer[:importance] = params[:user_answer][:importance]
+    else
+        flash[:notice] = "Does Not Exist"
+        @user = User.find(params[:user_answer][:user_id])
+        @user_answer = @user.user_answers.build(:user_id => params[:user_answer][:user_id], :question_id => params[:user_answer][:question_id], :answer => params[:user_answer][:answer], :importance => params[:user_answer][:importance])
+    end
+    logger.debug "User Answer: #{@user_answer.attributes.inspect}"
     if @user_answer.valid?
         if @user_answer.save
             flash[:notice] = "Thanks bro, got your answer"
             redirect_to questions_path
         else
             flash[:error] = "Couldnt save answer"
+            redirect_to questions_path
         end
     else
-        render :action => 'new'
+        flash[:error] = "Not a valid answer => #{@user_answer.attributes.inspect}"
+        redirect_to questions_path
     end
   end
 
-  #protected
-  #  def find_and_replace_answer
-  #  end
 
 end
--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -1,8 +1,8 @@
 class UserAnswer < ActiveRecord::Base
-  attr_accessible :answer_id, :importance, :question_id, :user_id
+  attr_accessible :answer, :importance, :question_id, :user_id
   belongs_to :user
   belongs_to :question
-  validates :answer_id, presence: true
+  validates :answer, presence: true
   validates :importance, presence: true
   validates :question_id, presence: true
   validates :user_id, presence: true
